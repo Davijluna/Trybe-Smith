56c63f08dabca047daeb8841fc0f7886b26a2c7d
davi <davijluna@gmail.com>
2022-09-18 20:24:59 -0300

    fix: teste de arquivos

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express, { Request, Response } from 'express'; //
 import ProductController from './controllers/productController'; //
 import UserController from './controllers/userController';
 
+import {
+  checkUsername,
+  checkClasse,
+  checkLevel,
+  checkPassword } from './middlewares/userMiddleware';
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +18,13 @@ const userController = new UserController();
 app.get('/', (_req: Request, res: Response) => res.json({ ok: true }));
 app.post('/products', productController.create); ///
 app.get('/products', productController.getAll);
-app.post('/users', userController.create);
+app.post(
+  '/users',
+  checkUsername,
+  checkClasse,
+  checkLevel,
+  checkPassword,
+  userController.create,
+);
 
 export default app;
---
+++ src/middlewares/userMiddleware.ts
@@ -0,0 +1,68 @@
+import { NextFunction, Request, Response } from 'express';
+import Users from '../interface/Users .Interfaces';
+/// 
+
+export const checkUsername = async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as Users;
+  if (!user.username) {
+    return res.status(401).json({ message: '"username" is require' });
+  }
+  if (user.username.length <= 2) {
+    return res
+      .status(422).json({ 
+        message: '"username" length must be at least 3 characters long',
+      });
+  }
+  if (typeof user.username !== 'string') {
+    return res.status(422).json({ message: '"username" is not a string' });
+  }
+  next();
+};
+
+export const checkClasse = async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as Users;
+  if (!user.classe) {
+    return res.status(401).json({ message: '"classe" is require' });
+  }
+  if (user.classe.length <= 2) {
+    return res
+      .status(422).json({ 
+        message: '"classe" length must be at least 3 characters long',
+      });
+  }
+  if (typeof user.classe !== 'string') {
+    return res.status(422).json({ message: '"classe" is not a string' });
+  }
+  next();
+};
+
+export const checkLevel = async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as Users;
+  if (!user.level) {
+    return res.status(401).json({ message: '"level" is require' });
+  }
+  if (user.level <= 0) {
+    return res
+      .status(422).json({ 
+        message: '"level" length must be at least 3 characters long',
+      });
+  }
+  if (typeof user.level !== 'number') {
+    return res.status(422).json({ message: '"level" is not a number' });
+  }
+  next();
+};
+
+export const checkPassword = async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as Users;
+  if (!user.password) {
+    return res.status(401).json({ message: '"password" is require' });
+  }
+  if (user.password <= 0) {
+    return res
+      .status(422).json({ 
+        message: '"password" length must be at least 3 characters long',
+      });
+  }
+  next();
+};
