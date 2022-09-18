b0d6af9a47f3ba23db85182ddd886d6480ef1eee
davi <davijluna@gmail.com>
2022-09-18 02:44:51 -0300

    fix comferindo todos arquivos

---
+++ src/routers/products.router.ts
@@ -0,0 +1,11 @@
+// import { Router } from 'express';
+// import ProductController from '../controllers/userController';
+
+// const ProductController = userController;
+
+// const router = Router();
+
+// router.post('/products',);
+// // router.get('/products', getAll);
+
+// export default router;
