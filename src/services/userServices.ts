7c1bb7aefa9924088b316e08d2516381498dac77
davi <davijluna@gmail.com>
2022-09-18 15:34:48 -0300

    feat:implementando camada de services

---
+++ src/services/userServices.ts
@@ -0,0 +1,18 @@
+import connection from '../models/connection';
+import UserModel from '../models/userModel';
+import Users from '../interface/Users .Interfaces';
+
+class UserService {
+  private model: UserModel;
+
+  constructor() {
+    this.model = new UserModel(connection);
+  }
+
+  public create(product: Users): Promise<Users> {
+    const users = this.model.createUser(product);
+    return users;
+  }
+}
+
+export default UserService;
