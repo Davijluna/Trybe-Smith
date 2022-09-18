868fa5e08257bc9e32630835000be7c867b7d01a
davi <davijluna@gmail.com>
2022-09-18 15:52:16 -0300

    feat: implementado camada de controller para user

---
+++ src/controllers/userController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import UserService from '../services/productServices';
+
+class UserController {
+  constructor(private userService = new UserService()) { }
+
+  public create = async (req: Request, res: Response) => {
+    const result = req.body;
+    const product = await this.userService.create(result);
+    res.status(201).json(product);
+  };
+}
+
+export default UserController;
