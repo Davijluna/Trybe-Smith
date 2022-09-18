baf68016b7657f24f96730f61d7c684e29a46f4d
davi <davijluna@gmail.com>
2022-09-18 02:30:17 -0300

    feat: adicionando arquivo app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express'; //
+import ProductController from './controllers/userController'; //
 
 const app = express();
 
 app.use(express.json());
+
+const userController = new ProductController();
+
+app.get('/', (_req: Request, res: Response) => res.json({ ok: true }));
+app.post('/products', userController.create); ///
 
 export default app;
