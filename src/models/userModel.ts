eef99ee517c6aa5adfb72b852bd6188b040feef4
davi <davijluna@gmail.com>
2022-09-19 14:28:47 -0300

    fix: modificacao na maneira de desestruturacao nas linhas 18 , 19 e 20

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,17 +1,21 @@
-import { ResultSetHeader } from 'mysql2/promise';
-import connection from './connection';
-import Users from '../interface/Users .Interfaces';
+import { ResultSetHeader, Pool } from 'mysql2/promise';
+import Users from '../interface/Users .Interface';
+// import connection from './connection';
 
 export default class UserModel {
-  private connection = connection;
+  private connection : Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
 
   public async create(user: Users): Promise<Users> {
     const { username, classe, level, password } = user;
-    const result = await this.connection.execute<ResultSetHeader>(
+    const [result] = await this.connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?,?,?,?)',
       [username, classe, level, password],
     );
-    const [dataInserted] = result;
+    const dataInserted = result;
     const { insertId: id } = dataInserted;
     return { id, ...user } as Users;
   }
