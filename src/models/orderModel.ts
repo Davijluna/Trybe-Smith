b33ee655cd1f94f86625265c70af4a9a34ca934b
davi <davijluna@gmail.com>
2022-09-19 19:45:11 -0300

    fix: modificando ordermodel

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -1,5 +1,5 @@
 import { Pool } from 'mysql2/promise';
-import Orders from '../interface/Products.Interface';
+import Orders from '../interface/OrderInterface';
 
 export default class OrersModel {
   private connection: Pool;
@@ -8,10 +8,16 @@ export default class OrersModel {
     this.connection = connection;
   }
 
-  public async getAll(): Promise<Orders[]> {
-    const constOrders = await this.connection
-      .execute('SELECT * FROM Trybesmith.Orders');
-    const [rows] = constOrders;
-    return rows as Orders[];
+  public async getById(): Promise<Orders[]> { //
+    // https://dev.mysql.com/blog-archive/mysql-8-0-labs-json-aggregation-functions/ JSON_ARRAYAGG
+    const query = `
+    SELECT orders.id, orders.userId, JSON_ARRAYAGG(product.id) AS productsIds
+    FROM Trybesmith.Orders AS orders
+    JOIN Trybesmith.Products AS product
+    ON orders.id = product.orderId
+    GROUP BY orders.id
+    ORDER BY orders.userId`;
+    const [result] = await this.connection.execute(query);
+    return result as Orders[]; ///
   }
 }
